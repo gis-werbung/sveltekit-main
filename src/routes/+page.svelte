43d074471eea7c85e8a77695d1a9f997b5c65468
rainonwire <168972213+rainonwire@users.noreply.github.com>
2026-09-01 10:23:10 +0200

    FICK DIC

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,1 @@
-<h1>Wilkommen zu gis-werbung (oder so)</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<img src="https://en.meming.world/images/en/5/5e/Bald_Guy_Staring.jpg" alt="hi" />
