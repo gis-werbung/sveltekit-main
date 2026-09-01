c560ce459a725cf3ef56d28965f6f026983e010f
tabbedDev <83753407+tabbeddev@users.noreply.github.com>
2026-09-01 10:24:31 +0200

    Revert "FICK DIC"
    This reverts commit 43d074471eea7c85e8a77695d1a9f997b5c65468.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,2 @@
-<img src="https://en.meming.world/images/en/5/5e/Bald_Guy_Staring.jpg" alt="hi" />
+<h1>Wilkommen zu gis-werbung (oder so)</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
